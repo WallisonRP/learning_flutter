ad574834bdb9caffe676a5d7a363175850f2b6af
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-06 20:30:52 -0300

    working with alignments

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -5,33 +5,20 @@ void main() {
     //debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     home: Container(
-      // color: Colors.white,
       decoration: BoxDecoration(
-        border: Border.all(
-          width: 3,
-          color: Colors.white
-        ),
+        border: Border.all(width: 3, color: Colors.white),
       ),
-      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-      margin: EdgeInsets.all(0),
-      child: Column(
+      margin: EdgeInsets.only(top: 40),
+      child: Row(
         // ignore: prefer_const_literals_to_create_immutables
         children: <Widget>[
           // ignore: prefer_const_constructors
-          /*Text(
-            "A certificação de metodologias que nos auxiliam a lidar com o entendimento das metas propostas representa uma abertura para a melho",
-            textAlign: TextAlign.justify,
-            )*/
-
-          Text("t1"),
-
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Text("t2"), 
-          ),
-
-          Text("t3"),
+          Text('T1'),
+          Text('T2'),
+          Text('T3'),
         ],
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
       ),
     ),
   ));
