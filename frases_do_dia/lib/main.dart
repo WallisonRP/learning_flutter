6278d2f8689afe6bc4cd98ce42b80fb677bfe0a2
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-06 22:26:57 -0300

    using Stateful Widget and SetState Class for change variable and show in screen

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,8 +3,42 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: HomeStateful(),
   ));
+}
+
+class HomeStateful extends StatefulWidget {
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+    var _texto = "Wallison Franklin";
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.pink,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                  _texto = "Curso Flutter";
+                  });
+                },
+                style: ElevatedButton.styleFrom(primary: Colors.pink),
+                child: Text("Clique aqui")),
+            Text("Nome: $_texto")
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class Home extends StatelessWidget {
