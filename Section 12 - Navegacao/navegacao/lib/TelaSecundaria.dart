31fd5a64719aeac0d53eb75639e549fd2ac55d8f
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-11 19:15:12 -0300

    create app navegacao

---
+++ Section 12 - Navegacao/navegacao/lib/TelaSecundaria.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaSecundaria extends StatefulWidget {
+  const TelaSecundaria({Key? key}) : super(key: key);
+
+  @override
+  State<TelaSecundaria> createState() => _TelaSecundariaState();
+}
+
+class _TelaSecundariaState extends State<TelaSecundaria> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
---
+++ Section 12 - Navegacao/navegacao/lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'TelaSecundaria.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
