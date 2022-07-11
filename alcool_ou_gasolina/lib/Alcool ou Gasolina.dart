424fd304b46fba039f57787d3fd7cb365ab7f2f5
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-10 21:18:33 -0300

    create logic for AlcoolOuGasolina app

--- alcool_ou_gasolina/lib/Alcool ou Gasolina.dart
+++ alcool_ou_gasolina/lib/Alcool ou Gasolina.dart
@@ -3,92 +3,100 @@
 import 'package:flutter/material.dart';
 
 class AlcoolOuGasolina extends StatefulWidget {
-  const AlcoolOuGasolina({Key? key}) : super(key: key);
-
   @override
   State<AlcoolOuGasolina> createState() => _AlcoolOuGasolinaState();
 }
 
 class _AlcoolOuGasolinaState extends State<AlcoolOuGasolina> {
-
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  void _calcular() {
+    double? precoAlcool = double.tryParse(_controllerAlcool.text);
+    double? precoGasolina = double.tryParse(_controllerGasolina.text);
+
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textoResultado =
+            "Número inválido, digite números maiores que 0 e utilizando (.) ";
+      });
+    } else {
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina";
+        });
+      } else {
+        setState(() {
+          _textoResultado = "Melhor abastecer com alcool";
+        });
+      }
+      // _limparCampos();
+    }
+  }
+
+  void _limparCampos() {
+    _controllerAlcool.text = "";
+    _controllerGasolina.text = "";
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Álcool ou Gasolina")
-      ),
+      appBar: AppBar(title: Text("Álcool ou Gasolina")),
       body: Container(
         child: SingleChildScrollView(
           padding: EdgeInsets.all(32),
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-          Padding(
-            padding: EdgeInsets.only(bottom: 32),
-            child: Image.asset("images/logo.png"),
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 10),
-            child: Text(
-              "Saiba qual a melhor opção para abastecer seu carro",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 32),
+              child: Image.asset("images/logo.png"),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                "Saiba qual a melhor opção para abastecer seu carro",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Preço Alcool, ex: 3,89",
               ),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Preço Alcool, ex: 3,89",
+              style: TextStyle(fontSize: 22),
+              controller: _controllerAlcool,
             ),
-            style: TextStyle(
-              fontSize: 22
-            ),
-            controller: _controllerAlcool,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Preço Gasolina, ex: 5,89",
-            ),
-            style: TextStyle(
-              fontSize: 22
-            ),
-            controller: _controllerGasolina,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10),
-            child: ElevatedButton(
-            onPressed: (){}, 
-            style: ElevatedButton.styleFrom(
-              primary: Colors.blue,
-              onPrimary: Colors.white,
-              padding: EdgeInsets.all(15)
-            ),
-            child: Text(
-              "Calcular",
-              style: TextStyle(
-                fontSize: 20
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Preço Gasolina, ex: 5,89",
               ),
-              )
+              style: TextStyle(fontSize: 22),
+              controller: _controllerGasolina,
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              "Resultado",
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: ElevatedButton(
+                  onPressed: _calcular,
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      padding: EdgeInsets.all(15)),
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 20),
+                  )),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Text(
+                _textoResultado,
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-              ),
-          )
-      ]
-      ),
+            )
+          ]),
         ),
       ),
     );
