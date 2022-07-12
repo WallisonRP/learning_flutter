ff345ff2d5df9e430c35e69df993f1c88842f7c0
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-11 22:28:28 -0300

    create logic for cara_ou_coroa app

--- Section 12 - Navegacao/cara_ou_coroa/lib/HomePage.dart
+++ Section 12 - Navegacao/cara_ou_coroa/lib/HomePage.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:cara_ou_coroa/Resultado.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,9 +10,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  void _exibirResultado(){
-
+  void _exibirResultado() {
+    var _itens = ["cara", "coroa"];
+    var _numero = Random().nextInt(_itens.length);
+    var _resultado = _itens[_numero];
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Resultado(_resultado)));
   }
 
   @override
@@ -16,18 +23,21 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Color(0xff61bd86),
       body: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(padding: EdgeInsets.only(bottom: 40), child: Image.asset("images/logo.png"),),
-          GestureDetector(
-            onTap: _exibirResultado,
-            child: Image.asset("images/botao_jogar.png"),
-          )
-        ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Image.asset("images/logo.png"),
+            ),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
       ),
-    ),
     );
   }
 }
