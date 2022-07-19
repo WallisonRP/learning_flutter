be22aa8824b0b267ad81cc8c79b2b689a60518e4
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-19 18:00:15 -0300

    using shared_preferences

--- Section 14 - Manipulacao de dados/manipulacao_dados_preferencias/lib/Home.dart
+++ Section 14 - Manipulacao de dados/manipulacao_dados_preferencias/lib/Home.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,15 +11,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
+  String _textoSalvo = "Nada salvo";
   TextEditingController _controleDeCampo = TextEditingController();
 
-  _salvar() {
-
+  _salvar() async {
+    String valorDigitado = _controleDeCampo.text;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("nome", valorDigitado);
+    print("Valor salvo: $valorDigitado");
   }
 
-  _recuperar() {
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _textoSalvo = prefs.getString("nome") ?? "Sem valor";
+    });
+    print("Valor recuperado: $_textoSalvo");
+  }
 
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("nome");
+    print("Texto removido");
   }
 
   @override
@@ -31,18 +45,35 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.all(32),
         child: Column(
           children: [
-            Text("", style: TextStyle(fontSize: 20),),
+            Text(
+              _textoSalvo,
+              style: TextStyle(fontSize: 20),
+            ),
             TextField(
               keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Digite algo"
-              ),
+              decoration: InputDecoration(labelText: "Digite algo"),
               controller: _controleDeCampo,
             ),
             Row(
               children: [
-                ElevatedButton(onPressed: _salvar, child: Text("Salvar", style: TextStyle(fontSize: 20),), style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),),
-                ElevatedButton(onPressed: _recuperar, child: Text("Recuperar", style: TextStyle(fontSize: 20)), style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),),
+                ElevatedButton(
+                  onPressed: _salvar,
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),
+                ),
+                ElevatedButton(
+                  onPressed: _recuperar,
+                  child: Text("Recuperar", style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),
+                ),
+                ElevatedButton(
+                  onPressed: _remover,
+                  child: Text("Remover", style: TextStyle(fontSize: 20)),
+                  style: ElevatedButton.styleFrom(padding: EdgeInsets.all(15)),
+                ),
               ],
             )
           ],
