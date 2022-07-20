3e5e5a82b2ca607b02c5601b424f7b26cbc00796
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-20 15:59:08 -0300

    create list_to_do project

---
+++ Section 14 - Manipulacao de dados/list_to_do/lib/main.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
