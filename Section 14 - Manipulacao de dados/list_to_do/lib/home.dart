71cb25b1db08e17ae8b9f12f31d8f26b7567ee7a
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-20 18:21:10 -0300

    create ui

--- Section 14 - Manipulacao de dados/list_to_do/lib/home.dart
+++ Section 14 - Manipulacao de dados/list_to_do/lib/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,8 +10,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = ["Ir ao mercado", "Estudar", "Exercício do dia"];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context), 
+                    child: Text("Cancelar")
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        }, 
+                    child: Text("Salvar")
+                    )
+                  ],
+                );
+              });
+        },
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaTarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_listaTarefas[index]),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
