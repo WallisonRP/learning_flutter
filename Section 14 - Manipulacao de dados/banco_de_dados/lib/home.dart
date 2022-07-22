fd49c9a09573c70c40fa0cdb23129e5ceda33ef5
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-21 22:06:30 -0300

    create project for learning database

---
+++ Section 14 - Manipulacao de dados/banco_de_dados/lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  _recuperarBancoDeDados() async {
+    final caminhoBancoDados = await getDatabasesPath();
+    final localBancoDados = join(caminhoBancoDados, "banco.db");
+
+    var retorno = await openDatabase(
+      localBancoDados,
+      version: 1,
+      onCreate: (db, dbVersaoRecente) {
+        String sql =
+            "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER)";
+        db.execute(sql);
+      },
+    );
+
+    print("aberto: ${retorno.isOpen.toString()}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _recuperarBancoDeDados();
+    return Container();
+  }
+}
