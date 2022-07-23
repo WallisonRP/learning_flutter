c61ac9d01fa6c1c19d643a3fe62f6a06f1e61f07
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-23 16:02:55 -0300

    create project and interface for minhas_anotacoes app

---
+++ Section 14 - Manipulacao de dados/minhas_anotacoes/lib/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _descriptionController = TextEditingController();
+  _exibirTelaCadastro() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Adicionar anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _titleController,
+                  autofocus: true,
+                  decoration: const InputDecoration(
+                    labelText: "Título",
+                    hintText: "Digite o título..."
+                  ),
+                ),
+                TextField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(
+                    labelText: "Descrição",
+                    hintText: "Digite a descrição..."
+                  ),
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context), 
+                child: Text("Cancelar")
+                ),
+                TextButton(
+                onPressed: () {
+                  //salvar entrada do usuário
+
+                }, 
+                child: Text("Salvar")
+                )
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas Anotações"),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          child: const Icon(Icons.add),
+          onPressed: () {
+            _exibirTelaCadastro();
+          }),
+    );
+  }
+}
