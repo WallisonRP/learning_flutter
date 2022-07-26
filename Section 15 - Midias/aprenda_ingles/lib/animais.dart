f21c3fd22cae7492d4e695553df9a925b1606608
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-26 18:09:53 -0300

    configure items in GridView

--- Section 15 - Midias/aprenda_ingles/lib/animais.dart
+++ Section 15 - Midias/aprenda_ingles/lib/animais.dart
@@ -12,11 +12,32 @@ class _TabAnimaisState extends State<TabAnimais> {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
+      childAspectRatio: 1.2,
       children: [
         GestureDetector(
           onTap: (){},
-          child: Text("a"),
-        )
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){},
+          child: Image.asset("assets/images/vaca.png"),
+        ),
       ],
       );
   }
