02c097eac0f9781949523a087c6b1ddba34f793b
Wallyz <80826888+WallisonRP@users.noreply.github.com>
2022-07-26 21:15:58 -0300

    created project and added plugin to executando_videos app

---
+++ Section 15 - Midias/executando_videos/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
